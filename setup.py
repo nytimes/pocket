fbdb7e96cbd495dfdd40538a9299ac9d648d9ce6
Silas Ray <silas.ray@gmail.com>
2014-04-02 14:59:53 -0400

    PyPI prep
    Adding additional fields in preparation for PyPI submission.

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup, find_packages
 
-setup(name='pocket',
-      version='0.0.0',
+setup(name='sneeze-pocket',
+      version='0.0.1',
+      author='Silas Ray',
+      author_email='silas.ray@nytimes.com',
+      license='Apache2.0',
+      url='http://pocket.readthedocs.org/',
+      description='A Sneeze plugin for logging logging',
+      long_description='A plugin for Sneeze that shunts logging messages to the reporting DB.',
+      classifiers=['Development Status :: 4 - Beta',
+                   'Intended Audience :: Information Technology',
+                   'Topic :: Software Development :: Quality Assurance',
+                   'Topic :: Software Development :: Testing'],
       packages=find_packages(),
-      install_requires=['sneeze'],
+      install_requires=['nose-sneeze'],
       entry_points={'nose.plugins.sneeze.plugins.add_models' : ['pocket_models = pocket.database:add_models'],
                     'nose.plugins.sneeze.plugins.add_options' : ['pocket_options = pocket.log_lib:add_options'],
                     'nose.plugins.sneeze.plugins.managers' : ['pocket_manager = pocket.log_lib:TissueHandler']})
